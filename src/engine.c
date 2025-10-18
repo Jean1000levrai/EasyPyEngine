7857f25b20190139ba074854129f7633bdb1e552
Jean Salomon <jean2salomon@icloud.com>
2025-10-18 22:18:08 +0200

    minus cmakelist from repo

--- src/engine.c
+++ src/engine.c
@@ -1,3 +1,9 @@
 #include <SDL2/SDL.h>
 
 // clang main.c -o main $(sdl2-config --cflags --libs)
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Quit();
+    return 0;
+}
