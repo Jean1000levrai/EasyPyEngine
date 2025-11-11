6018a987499cff82216961743b8b12a9d56a7ecb
Jean Salomon <jean2salomon@icloud.com>
2025-11-11 17:55:17 +0100

    release macos

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append("python/easyPyEngine")
-import easyPyEngine
+# sys.path.append("python/easyPyEngine")
+import easyPyEngine.easyPyEngine as easyPyEngine
 print(dir(easyPyEngine))
 e = easyPyEngine.Engine("My Game", 800, 600)
+# 
